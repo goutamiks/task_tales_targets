4ebdfc4ef6e6fddc74e5ea8e1187ce6a62f15885
goutamiks <goutamikodase@gmail.com>
2024-03-09 15:06:30 +0530

    updated db name and password

--- server/config/database.js
+++ server/config/database.js
@@ -6,8 +6,8 @@ const mysql = require("mysql2");
 const connection = mysql.createConnection({
   host: "localhost",
   user: "root", // Replace with your MySQL username
-  password: "26G11nov#", // Replace with your MySQL password
-  database: "myProdDB", // Replace with your MySQL database name
+  password: "26G032003gks", // Replace with your MySQL password
+  database: "TaskTalesTargets", // Replace with your MySQL database name
 });
 
 // Connect to the database
